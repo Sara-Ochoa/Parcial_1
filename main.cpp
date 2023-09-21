5eda7deaa3ca7095175039905634d568b359baed
SaraOchoa8a <88735518+SaraOchoa8a@users.noreply.github.com>
2023-09-21 16:10:17 -0500

    Se agrega la parte en imagen para que haga la secuencia y se realizan los patrones 1 y 3

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@ void publick();
 void imagen();
 void verificacion();
 void mostrarPatrones();
-void patronOffOn();
+void patronOn();
 void setPatron();
+void MatrizRombo();
+void MatrizRare();
 
 void setup(){
     Serial.begin(9600);
@@ -24,15 +26,15 @@ void loop(){
     else if(opcion==2){
         Serial.println("Ingrese una a una las posiciones leds a encender: ");
         imagen();
-        Serial.println("Mostrando patron...");
-        Serial.println();
-        Serial.println("Para interrumpir ingrese cualquier caracter: ");
-        while(Serial.available()==0);
+        Serial.println("Mostrando patron..."); Serial.println();
         setPatron();
-        patronOffOn();
+        patronOn();
     }
     else if(opcion==3){
         mostrarPatrones();
+        Serial.println("Mostrando patron..."); Serial.println();
+        setPatron();
+        patronOn();
     }
 
     publick();
@@ -53,6 +55,7 @@ void imagen(){
     //Esta funcion se encarga de ir encendiendo los leds que
     //quiera el usuario hasta que de esa manera se forme un
     //patron.
+    int espera = 0;
     int fila;
     int columna;
     char estado;
@@ -93,8 +96,19 @@ void imagen(){
         else{
             patron[fila-1][columna-1] = 0;
         }
-        patronOffOn();
+        patronOn();
+    }
+    Serial.println("Ingrese el intervalo de tiempo (segundos): ");
+    while(Serial.available()==0);
+    espera = static_cast<int>(Serial.parseFloat()*1000);
+    clearScreen();
 
+    //Hacer que imprima ese patron n veces con el tiempo indicado
+    for(int i=0; i<3; i++){
+        patronOn();
+        delay(espera);
+        patronOff();
+        delay(espera);
     }
 }
 
@@ -103,7 +117,13 @@ void verificacion(){
 }
 
 void mostrarPatrones(){
-    Serial.println("Aca se esta mostrando todos los patrones 1 a 1");
+    //Esta funcion es la encargada de mostrar la secuencia de
+    //patrones uno a uno.
+    int espera = 1000; // Tiempo de delay entre encendido y apagado en milisegundos
+    MatrizRombo();
+    delay(espera);
+    MatrizRare();
+    delay(espera);
 }
 
 void clearScreen(){
@@ -114,7 +134,7 @@ void clearScreen(){
 }
 
 
-void patronOffOn() {
+void patronOn() {
     // Envía el patrón al 74HC595 de derecha a izquierda.
     for (int i = 7; i >= 0; i--) {
         int fila_data = 0;
@@ -129,6 +149,29 @@ void patronOffOn() {
     digitalWrite(pinLatch, LOW);
 }
 
+void patronOff() {
+    uint8_t m0s[8][8];
+    //Inicializar una matriz con todas las posiciones en OFF
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            m0s[i][j]=0;
+        }
+    }
+    // Envía el patrón al 74HC595 de derecha a izquierda.
+    for (int i = 7; i >= 0; i--) {
+        int fila_data = 0;
+        for (int j = 7; j >= 0; j--) {
+            fila_data |= (m0s[i][j] << (7 - j));
+        }
+        shiftOut(pinData, pinClock, LSBFIRST, fila_data);
+    }
+
+    // Carga el patrón en los LEDs.
+    digitalWrite(pinLatch, HIGH);
+    digitalWrite(pinLatch, LOW);
+}
+
+
 void setPatron(){
     for(int i=0; i<8; i++){
         for(int j=0; j<8; j++){
@@ -138,7 +181,45 @@ void setPatron(){
 }
 
 
+void MatrizRombo() {
+    // Inicializar la matriz con 0s
+    setPatron();
 
+    // Llena la matriz patron con el patrón del rombo
+    for (int i = 0; i < 4; i++) {
+        for (int j = 3 - i; j <= 4 + i; j++) {
+            patron[i][j] = 1;
+            patron[7 - i][j] = 1;
+        }
+    }
+    patronOn();
+}
+
+
+void MatrizRare() {
+    //Inicializar la matriz con 1s
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            patron[i][j] = 1;
+        }
+    }
+
+    // Sobrescribe algunos elementos con 0s según tu patrón
+    for (int i = 0; i < 8; i += 4) {
+        for (int j = 2; j < 8; j += 3) {
+            patron[i][j] = 0;
+            patron[i + 1][j] = 0;
+        }
+    }
+
+    for (int i = 2; i < 8; i += 4) {
+        for (int j = 0; j < 8; j += 3) {
+            patron[i][j] = 0;
+            patron[i + 1][j] = 0;
+        }
+    }
+    patronOn();
+}
 
 
 
