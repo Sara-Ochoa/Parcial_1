e10723775b71330027f0f2c7c7f75e205d640d23
SaraOchoa8a <88735518+SaraOchoa8a@users.noreply.github.com>
2023-09-22 18:56:25 -0500

    Se unieron todas las funcionalidades en el main

--- main.cpp
+++ main.cpp
@@ -1,19 +1,38 @@
 //CODIGO EN ARDUINO
 ////////////////////////////////////////////////////////////////////////////////////////////////7
+const int pinData=2;
+const int pinLatch=3;
+const int pinClock=4;
+
+uint8_t **patron;
 int opcion=0;
-void publick();
-void imagen();
+const int filas = 8;
+const int columnas = 8;
+
+void publik();
 void verificacion();
+void imagen();
 void mostrarPatrones();
 void patronOn();
+void patronOff();
 void setPatron();
 void MatrizRombo();
+void MatrizX();
 void MatrizRare();
+void MatrizZZ();
+void clearScreen();
+void cleanser();
+void asignarMemoria();
 
 void setup(){
     Serial.begin(9600);
-    publick();
+    pinMode(pinData, OUTPUT);
+    pinMode(pinLatch, OUTPUT);
+    pinMode(pinClock, OUTPUT);
+
+    publik();
 }
+
 
 void loop(){
     while (Serial.available() == 0);
@@ -21,34 +40,73 @@ void loop(){
 
     clearScreen();
     if(opcion==1){
+        asignarMemoria();
         verificacion();
+        setPatron();
+        patronOn();
+        cleanser();
     }
     else if(opcion==2){
+        asignarMemoria();
         Serial.println("Ingrese una a una las posiciones leds a encender: ");
         imagen();
         Serial.println("Mostrando patron..."); Serial.println();
         setPatron();
         patronOn();
+        cleanser();
     }
     else if(opcion==3){
+        asignarMemoria();
         mostrarPatrones();
         Serial.println("Mostrando patron..."); Serial.println();
         setPatron();
         patronOn();
+        cleanser();
     }
+    publik();
 
-    publick();
 }
 
-void publick(){
-    //Esta funcion se encarga de mostrar el menu de opciones
+
+void publik(){
+    //Esta funcion se encarga de mostrar el menu de opciones al usuario.
     clearScreen();
     Serial.println("Menu:");
     Serial.println("1. Verificar funcionamiento de los leds.");
     Serial.println("2. Visualizar un patron.");
-    Serial.println("3. Visualizar secuencia de patrones.");
+    Serial.println("3. Visualizar una secuencia.");
     Serial.println("Escoja una opcion.");
 }
+
+void verificacion(){
+    int veces = 3; // Veces que se comprueba el ciclo de encendido y apagado de la matriz de LEDs
+    int espera = 500; // Tiempo de delay entre encendido y apagado en milisegundos
+    for (int ciclo = 0; ciclo < veces; ciclo++) { // Ciclo que se repite
+        // Enciende todos los LEDs para verificar que están funcionando correctamente
+        for(int i = 0; i < 8; i++){
+            shiftOut(pinData, pinClock, LSBFIRST, 255); // Cambio a MSBFIRST
+        }
+        digitalWrite(pinLatch, HIGH);
+        digitalWrite(pinLatch, LOW);
+
+        Serial.println("Matriz encendida."); Serial.println();
+
+        // Espera
+        delay(espera);
+
+        // Apaga todos los LEDs
+        for(int i = 0; i < 8; i++){
+            shiftOut(pinData, pinClock, LSBFIRST, 0); // Cambio a MSBFIRST
+        }
+        digitalWrite(pinLatch, HIGH);
+        digitalWrite(pinLatch, LOW);
+
+        // Espera
+        delay(espera);
+    }
+}
+
+
 
 
 void imagen(){
@@ -62,29 +120,30 @@ void imagen(){
 
     setPatron();
     while(true){
-        Serial.println("Ingrese la ubicacion del led a encender o apagar.");
+        Serial.println("Ingrese la ubicacion del led a encender(+) o apagar(-) o presione 0 para terminar.");
         Serial.print("Fila: "); Serial.read();
         while(Serial.available()==0);
         fila=Serial.parseInt();
+
+        if(fila==0){
+            clearScreen();
+            break;//Se rompe el ciclo infinito
+        }
         clearScreen();
 
-        Serial.println("Ingrese la ubicacion del led a encender o apagar.");
+        Serial.println("Ingrese la ubicacion del led a encender(+) o apagar(-) o presione 0 para terminar.");
         Serial.print("Fila: ");
         Serial.println(fila);
+
         Serial.print("Columna: ");
         Serial.read();
         while(Serial.available()==0);
         columna=Serial.parseInt();
         clearScreen();
 
-        Serial.println("Ingrese la ubicacion del led a encender o apagar.");
+        Serial.println("Ingrese la ubicacion del led a encender(+) o apagar(-) o presione 0 para terminar.");
         Serial.print("Fila: "); Serial.println(fila);
         Serial.print("Columna: "); Serial.println(columna);
-
-        if(fila==0 || columna==0){
-            clearScreen();
-            break;//Se rompe el ciclo infinito
-        }
         Serial.print("Estado: "); Serial.read();
         while(Serial.available()==0);
         estado=Serial.read();
@@ -97,6 +156,7 @@ void imagen(){
             patron[fila-1][columna-1] = 0;
         }
         patronOn();
+
     }
     Serial.println("Ingrese el intervalo de tiempo (segundos): ");
     while(Serial.available()==0);
@@ -112,9 +172,6 @@ void imagen(){
     }
 }
 
-void verificacion(){
-    Serial.println("Aca se esta mostrando la verificacion de la matriz");
-}
 
 void mostrarPatrones(){
     //Esta funcion es la encargada de mostrar la secuencia de
@@ -122,15 +179,12 @@ void mostrarPatrones(){
     int espera = 1000; // Tiempo de delay entre encendido y apagado en milisegundos
     MatrizRombo();
     delay(espera);
+    MatrizX();
+    delay(espera);
     MatrizRare();
     delay(espera);
-}
-
-void clearScreen(){
-    //Esta funcion da la sensacion de limpiar el monitor serial
-    for(int i=0; i<5; i++){
-        Serial.println();
-    }
+    MatrizZZ();
+    delay(espera);
 }
 
 
@@ -148,6 +202,7 @@ void patronOn() {
     digitalWrite(pinLatch, HIGH);
     digitalWrite(pinLatch, LOW);
 }
+
 
 void patronOff() {
     uint8_t m0s[8][8];
@@ -195,6 +250,20 @@ void MatrizRombo() {
     patronOn();
 }
 
+void MatrizX() {
+    // Llena la matriz con el patrón de una "X"
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (i == j || i == (7 - j)) {
+                patron[i][j] = 1;
+            } else {
+                patron[i][j] = 0;
+            }
+        }
+    }
+    patronOn();
+}
+
 
 void MatrizRare() {
     //Inicializar la matriz con 1s
@@ -222,29 +291,49 @@ void MatrizRare() {
 }
 
 
+void MatrizZZ() {
+    // Inicializar la matriz con 0s
+    setPatron();
+
+    // Cambiar 4 0s por 1s en las filas del 1 al 4
+    for (int i = 0; i < 4; i++) {
+        for (int j = i; j <= i + 3; j++) {
+            patron[i][j] = 1;
+        }
+    }
+
+    // Cambiar 4 0s por 1s en las filas del 5 al 8
+    for (int i = 4; i < 8; i++) {
+        for (int j = 6 - (i - 4); j >= 3 - (i - 4); j--) {
+            patron[i][j] = 1;
+        }
+    }
+    patronOn();
+}
 
 
+void clearScreen(){
+    //Esta funcion da la sensacion de limpiar el monitor serial
+    for(int i=0; i<5; i++){
+        Serial.println();
+    }
+}
 
 
+void cleanser() {
+    // Liberar la memoria utilizada por el arreglo bidimensional
+    for (int i = 0; i < filas; i++) {
+        delete[] patron[i];
+    }
+    delete[] patron;
 
+    patron = nullptr;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void asignarMemoria() {
+    //Se inicializa el arreglo dinamico bidimensional
+    patron = new uint8_t *[filas];
+    for (int i = 0; i < filas; i++) {
+        patron[i] = new uint8_t[columnas];
+    }
+}
